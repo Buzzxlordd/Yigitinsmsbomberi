d7af9ffd1019a31f947253c609692879492f9776
Buzzxlordd <147877125+Buzzxlordd@users.noreply.github.com>
2023-10-23 20:55:06 +0300

    Update yigitinsmsbomberi.py

--- yigitinsmsbomberi.py
+++ yigitinsmsbomberi.py
@@ -14,10 +14,16 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
+     ______                         _     
+    |  ____|                       | |    
+    | |__   _ __   ___  _   _  __ _| |__  
+    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
+    | |____| | | | (_) | |_| | (_| | | | |
+    |______|_| |_|\___/ \__,_|\__, |_| |_|
+                               __/ |      
+                              |___/      
     
-    🆂🅼🆂 🅱🅾🅼🅱🅰🆂🅸     
-    
-    Sms: {}           {}by {}@yigitmisinla\n  
+    Sms: {}           {}by {}@tingirifistik\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n\n 2- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
